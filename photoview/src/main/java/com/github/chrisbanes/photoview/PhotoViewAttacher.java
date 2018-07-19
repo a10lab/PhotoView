25261df93d03cba34c7302f69896cd7577b5bc74
k-ozam <maskedronin.jp@gmail.com>
2018-07-19 21:24:06 +0900

    refactor: deprecatedなメソッドを置き換え

--- photoview/src/main/java/com/github/chrisbanes/photoview/PhotoViewAttacher.java
+++ photoview/src/main/java/com/github/chrisbanes/photoview/PhotoViewAttacher.java
@@ -20,7 +20,6 @@ import android.graphics.Matrix;
 import android.graphics.Matrix.ScaleToFit;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
-import android.support.v4.view.MotionEventCompat;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
@@ -181,8 +180,8 @@ public class PhotoViewAttacher implements View.OnTouchListener,
                         return false;
                     }
 
-                    if (MotionEventCompat.getPointerCount(e1) > SINGLE_TOUCH
-                            || MotionEventCompat.getPointerCount(e2) > SINGLE_TOUCH) {
+                    if (e1.getPointerCount() > SINGLE_TOUCH
+                            || e2.getPointerCount() > SINGLE_TOUCH) {
                         return false;
                     }
 
