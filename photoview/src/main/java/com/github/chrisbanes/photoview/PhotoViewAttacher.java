911a41c097615aacff5af12ffabb6e1092521368
k-ozam <maskedronin.jp@gmail.com>
2018-07-19 21:25:55 +0900

    feature: ダブルタップ時の挙動を変更

--- photoview/src/main/java/com/github/chrisbanes/photoview/PhotoViewAttacher.java
+++ photoview/src/main/java/com/github/chrisbanes/photoview/PhotoViewAttacher.java
@@ -263,24 +263,27 @@ public class PhotoViewAttacher implements View.OnTouchListener,
                 return false;
             }
 
+            /**
+             * 元処理では「元サイズ--->拡大(中)--->拡大(大)--->元サイズ」
+             * なので、「元サイズ--->拡大(大)--->元サイズ」に修正
+             *
+             * @param ev
+             * @return
+             */
             @Override
             public boolean onDoubleTap(MotionEvent ev) {
-                try {
-                    float scale = getScale();
-                    float x = ev.getX();
-                    float y = ev.getY();
-
-                    if (scale < getMediumScale()) {
-                        setScale(getMediumScale(), x, y, true);
-                    } else if (scale >= getMediumScale() && scale < getMaximumScale()) {
-                        setScale(getMaximumScale(), x, y, true);
-                    } else {
-                        setScale(getMinimumScale(), x, y, true);
-                    }
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    // Can sometimes happen when getX() and getY() is called
+                if (mOnDoubleTapListener != null) {
+                    mOnDoubleTapListener.onDoubleTap();
                 }
 
+                float e = getScale();
+                float x = ev.getX();
+                float y = ev.getY();
+                if (e >= getMinimumScale() && e < getMaximumScale()) {
+                    setScale(getMaximumScale(), x, y, true);
+                } else {
+                    setScale(getMinimumScale(), x, y, true);
+                }
                 return true;
             }
 
